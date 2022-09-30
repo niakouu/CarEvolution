ec9d9d52c7964a34eded3d444aff6dca14c25758
Edelina Alieva <2145013@edu.vaniercollege.qc.ca>
2022-09-30 13:34:06 -0400

    Matrix object implemented

--- app/src/main/java/edu/vanier/neuralNetwork/Matrix.java
+++ app/src/main/java/edu/vanier/neuralNetwork/Matrix.java
@@ -9,5 +9,36 @@ package edu.vanier.neuralNetwork;
  * @author 2145013
  */
 public class Matrix {
+    private final int numberOfColumns;
+    private final int numberOfRows;
+    private final double[][] data;
+    private final double[] firstRow;
     
+    public Matrix(double[][] inputs) {
+        this.numberOfColumns = inputs[0].length;
+        this.numberOfRows = inputs.length;
+        this.data = inputs;
+        this.firstRow = inputs[0];
+    }
+
+    public int getNumberOfColumns() {
+        return this.numberOfColumns;
+    }
+
+    public int getNumberOfRows() {
+        return this.numberOfRows;
+    }
+
+    public double[][] getData() {
+        return this.data;
+    }
+
+    public double[] getFirstRow() {
+        return this.firstRow;
+    }
+    
+    public int getRowPositionForWhichAdditionOfValuesIsHighest() {
+        double heighest = 0.0;
+        return 0;
+    }
 }

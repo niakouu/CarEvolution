241c47f2434a43a4e0958412a9499a4cf96d3493
EnYi Hou <enyi.hou@gmail.com>
2022-10-01 09:40:12 -0400

    Testing features such as collision, Shape intersection, motion, coordinates, acceleration and many more features

---
+++ app/src/main/java/edu/vanier/motion-control-collision/java/ShapeTester/Controller.java
@@ -0,0 +1,175 @@
+package ShapeTester;
+
+import javafx.animation.AnimationTimer;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Button;
+import javafx.scene.shape.CubicCurve;
+import javafx.scene.shape.Shape;
+
+public class Controller implements Initializable {
+
+    private BooleanProperty wPressed = new SimpleBooleanProperty();
+    private BooleanProperty aPressed = new SimpleBooleanProperty();
+    private BooleanProperty sPressed = new SimpleBooleanProperty();
+    private BooleanProperty dPressed = new SimpleBooleanProperty();
+
+    private BooleanBinding keyPressed = wPressed.or(aPressed).or(sPressed).or(dPressed);
+
+    private int movementVariable = 2;
+
+    private int counter = 0;
+
+    @FXML
+    private Rectangle shape1;
+
+    @FXML
+    private Rectangle shape2;
+
+    @FXML
+    private CubicCurve curve1;
+
+    @FXML
+    private CubicCurve curve2;
+
+    @FXML
+    private AnchorPane scene;
+
+    ArrayList<Shape> dangers = new ArrayList<>();
+
+    @FXML
+    void start(ActionEvent event) {
+        shape1.setLayoutY(200);
+        shape1.setLayoutX(280);
+    }
+
+    AnimationTimer timer = new AnimationTimer() {
+        @Override
+        public void handle(long timestamp) {
+
+            if (wPressed.get()) {
+
+                shape1.setLayoutY(shape1.getLayoutY() - movementVariable);
+            }
+
+            if (sPressed.get()) {
+                shape1.setLayoutY(shape1.getLayoutY() + movementVariable);
+            }
+
+            if (aPressed.get()) {
+                shape1.setLayoutX(shape1.getLayoutX() - movementVariable);
+            }
+
+            if (dPressed.get()) {
+                shape1.setLayoutX(shape1.getLayoutX() + movementVariable);
+            }
+
+            boolean isTouching = false;
+            for (int i = 0; i < dangers.size() && isTouching == false; i++) {
+
+                if ((Shape.intersect(shape1, dangers.get(i))).getBoundsInLocal().getWidth() != -1) {
+                    isTouching = true;
+                }
+
+            }
+
+            if (isTouching) {
+
+                counter++;
+                System.out.println(counter);
+                if (wPressed.get()) {
+
+                    shape1.setLayoutY(shape1.getLayoutY() + movementVariable);
+                }
+
+                if (sPressed.get()) {
+                    shape1.setLayoutY(shape1.getLayoutY() - movementVariable);
+                }
+
+                if (aPressed.get()) {
+                    shape1.setLayoutX(shape1.getLayoutX() + movementVariable);
+                }
+
+                if (dPressed.get()) {
+                    shape1.setLayoutX(shape1.getLayoutX() - movementVariable);
+                }
+
+            }
+
+        }
+
+    };
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        movementSetup();
+
+        for (int i = 0; i < scene.getChildren().size(); i++) {
+
+            if (scene.getChildren().get(i) != shape1 && !(Button.class.isInstance(scene.getChildren().get(i)))) {
+                dangers.add((Shape) scene.getChildren().get(i));
+            }
+
+        }
+
+        keyPressed.addListener(((observableValue, aBoolean, t1) -> {
+            if (!aBoolean) {
+                timer.start();
+            } else {
+                timer.stop();
+            }
+        }));
+
+    }
+
+    public void movementSetup() {
+        scene.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.W) {
+                wPressed.set(true);
+            }
+
+            if (e.getCode() == KeyCode.A) {
+                aPressed.set(true);
+            }
+
+            if (e.getCode() == KeyCode.S) {
+                sPressed.set(true);
+            }
+
+            if (e.getCode() == KeyCode.D) {
+                dPressed.set(true);
+            }
+        });
+
+        scene.setOnKeyReleased(e -> {
+            if (e.getCode() == KeyCode.W) {
+                wPressed.set(false);
+            }
+
+            if (e.getCode() == KeyCode.A) {
+                aPressed.set(false);
+            }
+
+            if (e.getCode() == KeyCode.S) {
+                sPressed.set(false);
+            }
+
+            if (e.getCode() == KeyCode.D) {
+                dPressed.set(false);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/edu/vanier/rotation-acceleration/Controller.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ShapeTester;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+
+/**
+ *
+ * @author enyihou
+ */
+public class Controller {
+
+    Scene scene;
+    Car car;
+
+    boolean wPressed = false, aPressed = false, dPressed = false;
+
+    public Controller(Scene scene, Car car) {
+        this.scene = scene;
+        this.car = car;
+
+        listenKey();
+        animation.start();
+
+    }
+
+    AnimationTimer animation = new AnimationTimer() {
+        @Override
+        public void handle(long now) {
+
+                car.deccelerate();
+            
+
+            if (wPressed) {
+                car.accelerate();
+            }
+
+            if (aPressed) {
+                car.moveLeft();
+            }
+
+            if (dPressed) {
+                car.moveRight();
+            }
+        }
+
+    };
+
+    public void listenKey() {
+      
+
+        scene.setOnKeyPressed((event) -> {
+
+            if (event.getCode() == KeyCode.W) {
+                wPressed = true;
+            }
+            if (event.getCode() == KeyCode.A) {
+                aPressed = true;
+            }
+
+            if (event.getCode() == KeyCode.D) {
+                dPressed = true;
+            }
+
+        });
+
+        scene.setOnKeyReleased((event) -> {
+            
+            if (event.getCode() == KeyCode.R) {
+
+                System.out.println("Released");
+                car.setLayoutX(200);
+                car.setLayoutY(200);
+            }
+
+            if (event.getCode() == KeyCode.W) {
+                wPressed = false;
+                System.out.println("W pressed");
+            }
+            if (event.getCode() == KeyCode.A) {
+                aPressed = false;
+            }
+
+            if (event.getCode() == KeyCode.D) {
+                dPressed = false;
+            }
+
+        });
+
+    }
+
+}

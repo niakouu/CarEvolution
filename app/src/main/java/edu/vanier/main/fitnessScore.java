e073251d0e197bdb654f9c6031d26a5b3a4d423d
niakouu <edelina.alieva@gmail.com>
2022-11-07 12:55:28 -0500

    Update fitnessScore.java

--- app/src/main/java/edu/vanier/main/fitnessScore.java
+++ app/src/main/java/edu/vanier/main/fitnessScore.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.vanier.main;
-import edu.vanier.main.Car; 
+package edu.vanier.main; 
+import edu.vanier.car.Car;
 import java.util.HashMap;
 import javafx.scene.shape.Shape;
 

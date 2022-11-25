ffd92c415f34cd47e2666b8f2cae3ed460ba6e0f
annniiieee <2156116@edu.vaniercollege.qc.ca>
2022-11-25 15:23:50 -0500

    Cleaning code

--- app/src/main/java/edu/vanier/main/FXMLController.java
+++ app/src/main/java/edu/vanier/main/FXMLController.java
@@ -5,41 +5,80 @@
 package edu.vanier.main;
 
 import edu.vanier.animations.CarAnimations;
-import edu.vanier.neuralNetwork.NeuralNetworkDisplay;
+import edu.vanier.neuralNetwork.NeuralNetwork;
+import edu.vanier.objects.Car;
+import edu.vanier.objects.Point;
+import java.util.ArrayList;
+import java.util.Collections;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Shape;
 
 /**
  *
  * @author enyihou
  */
 public class FXMLController {
-    
+
+    private final static int NUMBER_CARS = 20;
+    private ArrayList<Car> eliminatedCars;
+    private ArrayList<Car> cars;
+    private ArrayList<Shape> shapeDangers;
+    private ArrayList<Point> fitnessScore;
+    private boolean fitnessSet = false;
+    private Label time;
+
     private CarAnimations timer;
-    
+
     @FXML
     private Pane root;
-    
+
     @FXML
     private Button btnSetFitnessScore;
-    
+
     @FXML
     private Button btnResetFitnessScore;
-    
+
     @FXML
     private Button btnKillCars;
-    
+
     @FXML
     private Button btnStart;
-    
+
     @FXML
-    private Button btnPause; 
-    
+    private Button btnPause;
+
     @FXML
-    void initialize(){
+    private Slider sliderCarSpeed;
+
+    @FXML
+    private Slider sliderNbrOfNeurons;
+
+    @FXML
+    private ChoiceBox choiceBoxLayers;
+
+    @FXML
+    private Slider sliderAngularVelocity;
+    
+     @FXML
+    private ChoiceBox choiceBoxMR; 
+
+    Double mutationRate[] = {0.0, 0.05, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 1.00};
+    ObservableList<Double> mutationRateElements = FXCollections.observableArrayList(mutationRate);
+   
+    
+
+    @FXML
+    void initialize() {
         this.timer = new CarAnimations(root);
 
+        //this.choiceBoxMR.setItems(mutationRateElements);
         this.btnStart.setDisable(false);
         this.btnKillCars.setDisable(true);
         this.btnPause.setDisable(true);
@@ -50,7 +89,7 @@ public class FXMLController {
     private void killCars() {
         this.timer.killAll();
     }
-    
+
     @FXML
     private void setFitnessScore() {
         this.root.onMouseClickedProperty();
@@ -61,7 +100,7 @@ public class FXMLController {
         this.btnResetFitnessScore.setDisable(false);
         this.btnSetFitnessScore.setDisable(true);
     }
-    
+
     @FXML
     private void pauseCars(){
         this.timer.stop();
@@ -71,6 +110,7 @@ public class FXMLController {
         this.btnResetFitnessScore.setDisable(false);
         this.btnSetFitnessScore.setDisable(true);
     }
+
     @FXML
     private void resetFitnessScore() {
         
@@ -91,6 +131,7 @@ public class FXMLController {
         this.btnKillCars.setDisable(false);
         this.btnResetFitnessScore.setDisable(true);
         this.btnSetFitnessScore.setDisable(true);
+
     }
     
 }
